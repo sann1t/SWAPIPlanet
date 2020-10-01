88a6a4a6a4c956d8f32688bf5e3b2cdc284ac98d
sann1t <sann1t@mail.ru>
2020-10-01 23:15:51 +0600

    added progress bar added check of the request

--- app/src/main/java/com/example/swapiapi/PlanetActivity.java
+++ app/src/main/java/com/example/swapiapi/PlanetActivity.java
@@ -1,10 +1,10 @@
 package com.example.swapiapi;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 
 import com.example.swapiapi.adapters.MyAdapter;
 import com.example.swapiapi.models.PlanetList;
@@ -29,15 +29,30 @@ public class PlanetActivity extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
+        // Set up progress before call
+        final ProgressDialog progressDoalog;
+        progressDoalog = new ProgressDialog(PlanetActivity.this);
+        progressDoalog.setMax(100);
+        progressDoalog.setMessage("Its loading....");
+        progressDoalog.setTitle("ProgressDialog bar example");
+        progressDoalog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        // show it
+        progressDoalog.show();
+
         NetworkService.getInstance().getSwapApi().getListPlanets().enqueue(new Callback<PlanetList>() {
             @Override
             public void onResponse(Call<PlanetList> call, Response<PlanetList> response) {
+                progressDoalog.dismiss();
+                if(!response.isSuccessful()) {
+                    return;
+                }
                 adapter = new MyAdapter(response.body());
                 recyclerView.setAdapter(adapter);
             }
 
             @Override
             public void onFailure(Call<PlanetList> call, Throwable t) {
+                progressDoalog.dismiss();
                 t.printStackTrace();
             }
         });
